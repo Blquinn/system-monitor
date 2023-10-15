805ead781790440e9f3d1aa419babb5ce22835a8
Benjamin Quinn <benjamin.quinn92@gmail.com>
2023-10-15 11:06:42 -0400

    Add CPU info

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
+use std::time::Duration;
+
+use slint::Timer;
+use sysinfo::{CpuExt, System, SystemExt};
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
+    let mut sys = System::new_all();
+    let timer = Timer::default();
+
     let ui_handle = ui.as_weak();
-    ui.on_request_increase_value(move || {
-        let ui = ui_handle.unwrap();
-        ui.set_counter(ui.get_counter() + 1);
+    timer.start(slint::TimerMode::Repeated, Duration::from_millis(500), move || {
+        sys.refresh_cpu();
+        let cpus = sys.cpus();
+        let total_cpu = cpus.iter().fold(0.0, |acc, cpu| acc + cpu.cpu_usage());
+        let avg_cpu = total_cpu / cpus.len() as f32;
+        ui_handle.unwrap().set_cpu_usage(avg_cpu);
     });
 
     ui.run()
