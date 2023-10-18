8c611a3210aae364d2fe72c359f7895b7bc767ce
Benjamin Quinn <benjamin.quinn92@gmail.com>
2023-10-18 14:06:51 -0400

    Add cpus chart

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,80 @@
-use std::time::Duration;
+use std::{time::Duration, rc::Rc};
 
-use slint::Timer;
+use slint::{VecModel, Model, SharedString};
 use sysinfo::{CpuExt, System, SystemExt};
 
 slint::include_modules!();
 
+fn vec_to_path(vec: &VecModel<f32>) -> String {
+    let mut path = String::new();
+
+    let mut last: Option<f32> = None;
+    for (x, y) in vec.iter().enumerate() {
+        if let Some(l) = last {
+            path.push_str(format!("M {} {} L {} {} ", x-1, 100.0-l, x, 100.0-y).as_str());
+        }
+
+        last = Some(y.clone());
+    }
+
+    path.push_str("Z");
+
+    path
+}
+
 fn main() -> Result<(), slint::PlatformError> {
-    let ui = AppWindow::new()?;
+    let app = AppWindow::new()?;
 
     let mut sys = System::new_all();
-    let timer = Timer::default();
 
-    let ui_handle = ui.as_weak();
-    timer.start(slint::TimerMode::Repeated, Duration::from_millis(500), move || {
-        sys.refresh_cpu();
-        let cpus = sys.cpus();
-        let total_cpu = cpus.iter().fold(0.0, |acc, cpu| acc + cpu.cpu_usage());
-        let avg_cpu = total_cpu / cpus.len() as f32;
-        ui_handle.unwrap().set_cpu_usage(avg_cpu);
+    // Updates thread
+    let ui_handle = app.as_weak();
+    std::thread::spawn(move || {
+        loop {
+            sys.refresh_cpu();
+            let cpus = sys.cpus();
+
+            let usages: Vec<f32> = cpus.iter().map(|c| c.cpu_usage()).collect();
+            let total_cpu: f32 = usages.iter().sum();
+            let avg_cpu = total_cpu / cpus.len() as f32;
+
+            ui_handle.upgrade_in_event_loop(move |handle| {
+                let model = handle.global::<CpuMonitorPageAdapter>().get_cpu_data();
+
+                let cpus_models = model.as_any()
+                    .downcast_ref::<VecModel<CpuData>>()
+                    .unwrap();
+
+                for (i, u) in usages.iter().enumerate() {
+                    match cpus_models.row_data(i) {
+                        Some(mut cpu_data) => {
+                            let samples_model = cpu_data.samples.as_any().downcast_ref::<VecModel<f32>>().unwrap();
+                            samples_model.push(u.clone());
+                            if samples_model.row_count() > 120 {
+                                samples_model.remove(0);
+                            }
+
+                            cpu_data.path = SharedString::from(vec_to_path(&samples_model));
+
+                            cpus_models.set_row_data(i, cpu_data);
+                        }
+                        None => {
+                            let model: Rc<VecModel<f32>> = Rc::new(VecModel::default());
+                            model.set_vec(vec![u.clone()]);
+                            cpus_models.push(CpuData{
+                                samples: model.into(),
+                                path: SharedString::from(""),
+                            });
+                        }
+                    }
+                }
+
+                handle.set_cpu_usage(avg_cpu);
+            }).unwrap();
+
+            std::thread::sleep(Duration::from_millis(500));
+        }
     });
 
-    ui.run()
+    app.run()
 }
